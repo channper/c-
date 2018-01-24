f082375ed423d7b3ce3a6f07e5b8ff9f61194d54
Kuraaku Azuma <azumakura@gmail.com>
2018-01-24 19:50:49 +0900

    初めてコミット

---
+++ App5/App5/App5/Adress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App5
+{
+    class Adress
+    {
+        public string state { get; set; }
+        public string stateName { get; set; }
+        public string city{ get; set; }
+        public string street { get; set; }
+
+        public Address()
+        {
+            this.state = "";
+            this.stateName = "";
+            this.city = "";
+            this.street = "";
+        }
+
+    }
+}
---
+++ App5/App5/App5/Core.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App5
+{
+    class Core
+    {
+        public static async Task<dynamic> GetAddressSerchResult (string address)
+            {
+            string pzNumber = address.Substring(0, 3);
+        string taNumber = address.Substring(3);
+        string queryString = "http://api.thin.net/jzip/X0401/JSON/" + pzNumber + "/" + taNumber + ".js";
+        dynamic results = await HttpService.getDataFormService(queryString).ConfigureAwait(false);
+
+        if((results != null)&&(!string.IsNullOrEmpty((string) results["state"wo])))
+        {
+        Address Ad = new Address();
+        Ad.state = (string) results["state"];
+        Ad.stateNmae = (string)results["stateNmae"];
+        Ad.city =(string)results["city"];
+        Ad.street=(string)results["street"];
+        return Ad;
+        }
+        else
+        {
+         return null;
+        }
+      }
+
+    }
+}
