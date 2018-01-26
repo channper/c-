05360f51686121c67e691398d97c99e7851fd789
Kuraaku Azuma <azumakura@gmail.com>
2018-01-26 20:24:10 +0900

    コミット

---
+++ App5/App5/App5/HttpService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App5
+{
+    class HttpService
+    {
+        public static async Task<dynamic>
+            getDateFormService(string queryString)
+        {
+            dynamic date = null;
+            HttpClient = new HttpClient();
+            var response = await client.GetAsync(queryString);
+
+            if((response != null) &&! ((int)response.StatusCode >= 400))
+                {
+                string json = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserrializeObject(json);
+            }
+            return data;
+        }
+    }
+}
